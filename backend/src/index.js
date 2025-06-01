0ceb2fd064b23d1ba70e9462ebdba210f3fc1e1b
Mehandi <mehandixaidi786@gmail.com>
2025-06-01 10:58:25 +0530

    backend completed all apis r written

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 import express from "express"
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port:`, PORT);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
+import "./test.css"
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
+      < div className="font-bold text-white">Tailwind is working!</div>
     </div>
   );
 }
